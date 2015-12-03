c6bd7d1a15a102fc502c51131863a690a4cb6805
Otto Giron <otto.giron@xoom.com>
2015-12-03 11:21:36 -0600

    Template update

--- client/app/controllers/todos-list.js
+++ client/app/controllers/todos-list.js
@@ -2,10 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 		todos: Ember.inject.controller(),
+		allTodos: Ember.computed.alias('todos.model'),
 		itemController: 'todo',
 		canToggle: function () {
 			var anyTodos = this.get('allTodos.length');
-			var isEditing = this.isAny('isEditing');
+			var isEditing = this.get('allTodos').isAny('isEditing');
 			return anyTodos && !isEditing;
 		}.property('allTodos.length', '@each.isEditing')
 	});
