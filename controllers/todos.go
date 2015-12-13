066d1f687634b9e6cb8fabe0051fed2e0c06553c
Otto Giron <otto.giron@xoom.com>
2015-12-13 17:49:41 -0600

    Implements create and getAll

--- controllers/todos.go
+++ controllers/todos.go
@@ -1,10 +1,15 @@
 package controllers
 
 import (
-	"fmt"
+	"encoding/json"
+	"io"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/ottogiron/chapi/server"
+	"github.com/ottogiron/gotodo/models"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
 // Todos handle tasks requests
@@ -15,6 +20,7 @@ type Todos struct {
 // Register registers this plugin in the server
 func (t *Todos) Register(server server.Server) {
 	server.HandleFunc("/todos", handleGet).Methods("GET")
+	server.HandleFunc("/todos", handlePOST).Methods("POST")
 }
 
 // Name identifier for controller
@@ -23,20 +29,50 @@ func (t *Todos) Name() string {
 }
 
 func handleGet(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, `
-		 [
-			{
-				"type": "todo",
-				"id": 1,
-				"title": "My First task",
-				"isCompleted": false
-			},
-			{
-				"type": "todo",
-				"id": 2,
-				"title": "My First Second task",
-				"isCompleted": true
-			}
-		]
-		`)
+	session, err := createMongoSession()
+	defer session.Close()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	collection := session.DB("todos").C("todos")
+	result := models.Todos{}
+	err = collection.Find(bson.M{}).All(&result)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(w).Encode(result)
+}
+
+func handlePOST(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
+	if err != nil {
+		panic(err)
+	}
+
+	todo := models.NewTodo()
+	if err := json.Unmarshal(body, todo); err != nil {
+		w.Header().Set("Content-Type", "application/vnd.api+json")
+		w.WriteHeader(422) // unprocessable entity
+		if err := json.NewEncoder(w).Encode(err); err != nil {
+			panic(err)
+		}
+	}
+
+	session, err := createMongoSession()
+	defer session.Close()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	collection := session.DB("todos").C("todos")
+	collection.Insert(todo)
+	json.NewEncoder(w).Encode(todo)
+
+}
+
+func createMongoSession() (*mgo.Session, error) {
+	return mgo.Dial("192.168.99.100")
 }
--- models/todo.go
+++ models/todo.go
@@ -1,7 +1,20 @@
 package models
 
+import "gopkg.in/mgo.v2/bson"
+
+//Todo todo model
 type Todo struct {
-	id          int
-	title       string
-	isCompleted bool
+	ID          bson.ObjectId `bson:"_id,omitempty" json:"id"`
+	Title       string        `json:"title"`
+	IsCompleted bool          `json:"isCompleted"`
+	MType       string        `json:"type"`
+}
+
+//Todos todo lists
+type Todos []Todo
+
+//NewTodo initializes a new todo
+func NewTodo() *Todo {
+	id := bson.NewObjectId()
+	return &Todo{ID: id, MType: "todo"}
 }
