03e74fc43ca209b61f0d767495f66275c7a8ffb8
Otto Giron <otto.giron@xoom.com>
2015-12-13 22:04:52 -0600

    Remove front end from project

--- client/app/components/todo-input.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.TextField.extend({
-		focusOnInsert:  () => {
-			// Re-set input value to get rid of a redundant text selection
-			this.$().val(this.$().val());
-			this.$().focus();
-		}.on('didInsertElement')
-	});
--- client/app/routes/todos/active.js
+++
@@ -1,8 +0,0 @@
-
-import TodosIndex from './index';
-
-export default TodosIndex.extend({
-		model() {
-			return this.store.filter('todo',  (todo) => !todo.get('isCompleted'));
-		}
-	});
